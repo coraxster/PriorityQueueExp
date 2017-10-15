035245317352786836bdaf64f9017bdc1129bc8c
dmitry kuzmin <corax@list.ru>
2017-10-15 22:47:37 +0300

    rework

--- main.go
+++ main.go
@@ -8,35 +8,29 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	//"math/rand"
-	"container/heap"
-	//"github.com/coraxster/PQueue"
+	"github.com/coraxster/PriorityQueue"
 )
 
-
-
+type Job struct {
+	str string
+}
 
 func main()  {
+	fmt.Println("Start!")
+	InChs := make([]chan interface{}, 0)
+	for priority := 0; priority <= 10 ; priority++  {
+		InChs = append(InChs, make(chan interface{}))
+	}
 
-	chan1in := make(chan *itemWithPriority)
-	chan2out, _ := prioritise2 (chan1in)
+	chan2out, _ := PriorityQueue.Prioritize(InChs...)
 	work(chan2out)
 
-	sl := make([]*itemWithPriority, 0, 1000)
 	for priority := 10; priority > 0 ; priority--  {
 		for i:=1; i<100; i++ {
-			item := itemWithPriority{
-				i: i,
-				item:"hi - pr:" + strconv.Itoa(priority) + " (" + strconv.Itoa(i) + ")",
-				priority: priority,
-			}
-			sl = append(sl, &item)
+			InChs[priority] <- "hi - pr:" + strconv.Itoa(priority) + " (" + strconv.Itoa(i) + ")"
 		}
 	}
 
-	for _, item := range sl {
-		chan1in <- item
-	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(make(chan os.Signal, 1), syscall.SIGINT, syscall.SIGTERM)
@@ -46,24 +40,31 @@ func main()  {
 
 
 
-func work(ch chan *itemWithPriority){
+
+func work(ch chan interface{}){
 	var m sync.Mutex
 	doneCount := 0
 	for i := 1; i <= 3; i++ {
 		go func(i int) {
 			for item := range ch{
-				data := item.item.(string)
+				data := item.(string)
 				m.Lock()
 				fmt.Println(strconv.Itoa(doneCount) + ". " + data + " - worker " + strconv.Itoa(i))
 				doneCount++
 				m.Unlock()
-				time.Sleep(time.Microsecond * 5 )
+				time.Sleep(time.Millisecond * 500 )
 			}
 		}(i)
 	}
 }
 
 
+
+
+
+
+
+//Legacy impl
 func prioritise(in chan *itemWithPriority) (chan *itemWithPriority, error) {
 	var queues [][]*itemWithPriority
 	var m sync.Mutex
@@ -112,80 +113,9 @@ func prioritise(in chan *itemWithPriority) (chan *itemWithPriority, error) {
 	return out, nil
 }
 
-func prioritise2(in chan *itemWithPriority)(chan *itemWithPriority, error)  {
-	out := make(chan *itemWithPriority)
-	var m sync.Mutex
-	pq := make(Queue, 0)
-	heap.Init(&pq)
-
-	go func() {
-		for item := range in {
-			m.Lock()
-			heap.Push(&pq, item)
-			m.Unlock()
-		}
-	}()
-
-	go func() {
-		for{
-			if pq.Len() > 0 {
-				m.Lock()
-				item := heap.Pop(&pq).(*itemWithPriority)
-				m.Unlock()
-				out <- item
-			}
-		}
-	}()
-
-	return out, nil
-}
-
-
 type itemWithPriority struct {
 	i int
 	index int
 	priority int
 	item     interface{}
-}
-
-// A Queue implements heap.Interface and holds Items.
-type Queue []*itemWithPriority
-
-func (pq Queue) Len() int { return len(pq) }
-
-func (pq Queue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	if pq[i].priority == pq[j].priority {
-		return pq[i].i < pq[j].i
-	}
-	return pq[i].priority < pq[j].priority
-}
-
-func (pq Queue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *Queue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*itemWithPriority)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *Queue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
-// update modifies the priority and value of an Item in the queue.
-func (pq *Queue) update(item *itemWithPriority, value string, priority int) {
-	item.item = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
 }
